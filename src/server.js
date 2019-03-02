3f87c72a85d00a7814110516ef8ff17df646f784
forjing <xiaodoubao1994@gmail.com>
2019-03-02 14:05:23 +0800

    add mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose')
 const express = require('express')
-
 const app = express()
+
+const PORT = 8888
 
 const mongo_url = process.env.mongodb_url
 
-console.log('mongo_url', mongo_url)
+mongoose.connect(mongo_url, { useNewUrlParser: true })
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('connect open!')
+
+  app.listen(PORT, () => {
+    console.log(`server is running at http://localhost:${PORT}`)
+  })
+})
